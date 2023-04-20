fc862c111d17557e55a9eca777511d15ccd87b53
Duniverse <wangari.dun@gmail.com>
2023-04-20 20:32:35 +0300

    adds expenses schema, route and controller

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,9 +1,14 @@
 const {addIncome, getIncomes, deleteIncomes} = require("../controllers/income")
+const {addExpense, getExpenses, deleteExpenses} = require("../controllers/expense")
 
 const router = require("express").Router()
 
 router.post("/add-income", addIncome)
     .get("/get-incomes", getIncomes)
     .delete("/delete-incomes/:id", deleteIncomes)
+    //expenses
+    .post("/add-expense", addExpense)
+    .get("/get-expenses", getExpenses)
+    .delete("/delete-expenses/:id", deleteExpenses)
 
 module.exports = router
